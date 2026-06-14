b3140206a20cae7edc70aea608f6fc29d4894fd7
Anuj Singh <anujmaurya7967@gmail.com>
2026-06-14 22:07:15 +0530

    deploy

--- backend/app/config.py
+++ backend/app/config.py
@@ -12,5 +12,8 @@ PORT = int(os.getenv("PORT", 8000))
 # In development this is localhost:5500 (Live Server) and localhost:8080
 ALLOWED_ORIGINS = os.getenv(
     "ALLOWED_ORIGINS",
-    "http://localhost:5500,http://127.0.0.1:5500,http://localhost:8080"
+    "http://localhost:5500,"
+    "http://127.0.0.1:5500,"
+    "http://localhost:8080,"
+    "https://p2p-file-transfer-six.vercel.app"
 ).split(",")
